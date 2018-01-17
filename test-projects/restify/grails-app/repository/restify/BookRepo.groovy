30b8b22a09671b7d6c792f7768c5cf0d64368184
alexeyzvegintcev <alexey.zvegintcev@sysgears.com>
2018-01-17 10:06:17 +0200

    Show spoc extending issue

--- test-projects/restify/grails-app/repository/restify/BookRepo.groovy
+++ test-projects/restify/grails-app/repository/restify/BookRepo.groovy
@@ -8,6 +8,7 @@ class BookRepo extends DefaultGormRepo<Book> {
 
     @Override
     Book create(Map params){
+        params.title += 1
         super.create(params)
     }
 
--- test-projects/restify/src/integration-test/groovy/restify/BookRestApiSpec.groovy
+++ test-projects/restify/src/integration-test/groovy/restify/BookRestApiSpec.groovy
@@ -4,6 +4,8 @@ import gorm.restapi.testing.RestApiFuncSpec
 import grails.testing.mixin.integration.Integration
 import grails.transaction.Rollback
 
+import static org.springframework.http.HttpStatus.CREATED
+
 @Integration
 @Rollback
 class BookRestApiSpec extends RestApiFuncSpec {
@@ -23,5 +25,28 @@ class BookRestApiSpec extends RestApiFuncSpec {
 
     Map getUpdateData() { [title: "project Update"] }
 
+    Map getNewInsertData(){
+        Map newMap = getInsertData()
+        newMap.title = newMap.title +1
+        newMap
+    }
+
+
+    void test_save_post() {
+        given:
+        def response
+
+        when: "The save action is executed with valid data"
+        response = restBuilder.post(resourcePath) {
+            json insertData
+        }
+
+        then: "The response is correct"
+        response.status == CREATED.value()
+        verifyHeaders(response)
+        subsetEquals(newInsertData, response.json)
+        def rget = restBuilder.get("$resourcePath/${response.json.id}")
+        subsetEquals(newInsertData, rget.json)
+    }
 
 }
